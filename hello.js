cad97ae53f9e06b950b230d73e41addd7bdd77cd
Arthur Miguel <arthur.miguel@alunos.ifsuldeminas.edu.br>
2025-08-21 16:10:24 -0300

    adicionando codigos ao repositorio

---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log("\nHELLO WORD!!!!!!!");
+
+console.log("----------------------");
---
+++ operadores.js
@@ -0,0 +1,12 @@
+let x 
+let y
+
+console.log(1 + 2);
+console.log("soma: " + (x + y);
+console.log("subtracao: " + (x - y));
+console.log("divisao (x/y)
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log("-----------");
+console.log("")
